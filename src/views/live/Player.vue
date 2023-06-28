064035f0520d44c86c1330a10f4f7da3dd308cfd
xyf2018 <33438484+xyf2018@users.noreply.github.com>
2023-06-28 22:37:10 +0800

    调整 弹幕样式和速度

--- src/views/live/Player.vue
+++ src/views/live/Player.vue
@@ -84,7 +84,9 @@ export default {
         live: true,
         hotkey: true,
         screenshot: true,
-        danmaku: true,
+        danmaku: {
+          speedRate: 0.5,
+        },
         video: {
           // url:"https://usher.ttvnw.net/api/channel/hls/tsm_imperialhal.m3u8?allow_source=true&dt=2&fast_bread=true&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=d0385e3ce933c8fe76ff6a9a4d9ce01e29b04f6b&supported_codecs=vp09%2Cavc1&token=%7B%22adblock%22%3Afalse%2C%22authorization%22%3A%7B%22forbidden%22%3Afalse%2C%22reason%22%3A%22%22%7D%2C%22blackout_enabled%22%3Afalse%2C%22channel%22%3A%22tsm_imperialhal%22%2C%22channel_id%22%3A146922206%2C%22chansub%22%3A%7B%22restricted_bitrates%22%3A%5B%5D%2C%22view_until%22%3A1924905600%7D%2C%22ci_gb%22%3Afalse%2C%22geoblock_reason%22%3A%22%22%2C%22device_id%22%3Anull%2C%22expires%22%3A1638161547%2C%22extended_history_allowed%22%3Afalse%2C%22game%22%3A%22%22%2C%22hide_ads%22%3Afalse%2C%22https_required%22%3Atrue%2C%22mature%22%3Afalse%2C%22partner%22%3Afalse%2C%22platform%22%3A%22web%22%2C%22player_type%22%3A%22site%22%2C%22private%22%3A%7B%22allowed_to_view%22%3Atrue%7D%2C%22privileged%22%3Afalse%2C%22role%22%3A%22%22%2C%22server_ads%22%3Atrue%2C%22show_ads%22%3Atrue%2C%22subscriber%22%3Afalse%2C%22turbo%22%3Afalse%2C%22user_id%22%3Anull%2C%22user_ip%22%3A%22203.175.12.116%22%2C%22version%22%3A2%7D&cdm=wv&player_version=1.4.0",
           // type:"hls"
@@ -320,7 +322,9 @@ export default {
 
 <style>
 .dplayer-danmaku .dplayer-danmaku-item {
-  text-shadow: 1px 1px 1.5px rgba(0, 0, 0, 1);
+  font-size: 1.5rem;
+  font-weight: bold;
+  -webkit-text-stroke: 0.5px black;
 }
 
 @media screen and (min-width: 0px) and (max-width: 500px) {
